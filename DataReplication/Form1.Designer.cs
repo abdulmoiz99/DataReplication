d99f3403c976d43f26456b4b5ead656db896776e
Abdul Moiz <moiza0799@gmail.com>
2020-09-03 00:27:27 +0500

    Directory Model

--- DataReplication/Form1.Designer.cs
+++ DataReplication/Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txt_DesPath = new System.Windows.Forms.TextBox();
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.btn_Copy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lab_Space = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txt_DesPath
+            // 
+            this.txt_DesPath.Location = new System.Drawing.Point(68, 21);
+            this.txt_DesPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_DesPath.Name = "txt_DesPath";
+            this.txt_DesPath.Size = new System.Drawing.Size(642, 26);
+            this.txt_DesPath.TabIndex = 2;
+            // 
+            // btn_Browse
+            // 
+            this.btn_Browse.Location = new System.Drawing.Point(716, 21);
+            this.btn_Browse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(97, 27);
+            this.btn_Browse.TabIndex = 3;
+            this.btn_Browse.Text = "Browse";
+            this.btn_Browse.UseVisualStyleBackColor = true;
+            this.btn_Browse.Click += new System.EventHandler(this.btn_Browse_Click);
+            // 
+            // btn_Copy
+            // 
+            this.btn_Copy.Location = new System.Drawing.Point(819, 21);
+            this.btn_Copy.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Copy.Name = "btn_Copy";
+            this.btn_Copy.Size = new System.Drawing.Size(97, 27);
+            this.btn_Copy.TabIndex = 4;
+            this.btn_Copy.Text = "Copy";
+            this.btn_Copy.UseVisualStyleBackColor = true;
+            this.btn_Copy.Click += new System.EventHandler(this.btn_Copy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Path: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(910, 313);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "";
+            // 
+            // lab_Space
+            // 
+            this.lab_Space.AutoSize = true;
+            this.lab_Space.Location = new System.Drawing.Point(70, 51);
+            this.lab_Space.Name = "lab_Space";
+            this.lab_Space.Size = new System.Drawing.Size(138, 20);
+            this.lab_Space.TabIndex = 7;
+            this.lab_Space.Text = "Available Space: ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(738, 521);
+            this.ClientSize = new System.Drawing.Size(934, 435);
+            this.Controls.Add(this.lab_Space);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Copy);
+            this.Controls.Add(this.btn_Browse);
+            this.Controls.Add(this.txt_DesPath);
+            this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txt_DesPath;
+        private System.Windows.Forms.Button btn_Browse;
+        private System.Windows.Forms.Button btn_Copy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lab_Space;
     }
 }
 
--- DataReplication/Form1.cs
+++ DataReplication/Form1.cs
@@ -1,20 +1,137 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace DataReplication
 {
     public partial class Form1 : Form
     {
+        string FolderPath = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    FolderPath = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btn_Copy_Click(object sender, EventArgs e)
+        {
+            txt_DesPath.SelectionLength = 0;
+
+
+            DirectoryModel.DirectoryCopy(@"" + Application.StartupPath + " \\Shared", txt_DesPath.Text, true);
+            MessageBox.Show("Files copied successfully", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_DesPath.Text = DirectoryModel.CheckDirectory(@"C:\Users\Public\Documents\SoundMagic\NeoOrchestra\Presets");
+            lab_Space.Text = DirectoryModel.displayAvailableSpace(txt_DesPath.Text);
+
+        }
+    }
+    static class DirectoryModel
+    {
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                if (File.Exists(temppath))
+                {
+
+                }
+                else
+                {
+                    file.CopyTo(temppath, false);
+
+                }
+
+
+            }
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+        private static string FormatBytes(long bytes)
+        {
+            string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return String.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+        public static string displayAvailableSpace(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string driveName = path.Substring(0, 3);
+                return "Available Space: " + FormatBytes(GetTotalFreeSpace(driveName));
+            }
+            else return "Available Space:";
+        }
+        public static string CheckDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return path;
+            }
+            else return string.Empty;
+        }
+        private static long GetTotalFreeSpace(string driveName)
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady && drive.Name == driveName)
+                {
+                    return drive.TotalFreeSpace;
+                }
+            }
+            return -1;
         }
     }
 }
